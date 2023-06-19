97175dc9dd7094b355cec1eb27878eb0b8bd8750
Iqbal Rahman <iqbal070103@gmail.com>
2023-06-19 17:14:58 +0700

    4. Update Dabatase

--- library/database.js
+++ library/database.js
@@ -1,10 +1,10 @@
 let mysql = require('mysql');
  
 let connection = mysql.createConnection({
-   host:        'https://databases.000webhost.com/',
-   user:        'id17203131_trans',
-   password:    'Iqbal2507.',
-   database:    'id17203131_transprima'
+   host:        'db4free.net',
+   user:        'transprima',
+   password:    'Iqbal2507',
+   database:    'transprima'
  });
 
 connection.connect(function(error){
