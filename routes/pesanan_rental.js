e3285441c5f56cdc16f7b623bb0c3bd92c661c3f
Iqbal Rahman <iqbal070103@gmail.com>
2023-06-22 15:27:46 +0700

    6. Update Field Catatan

--- routes/pesanan_rental.js
+++ routes/pesanan_rental.js
@@ -27,10 +27,10 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  const { id_akun, id_rental, customer, telp, alamat, tujuan, unit, lama, tanggal, tanggal_pesan, harga } = req.body;
+  const { id_akun, id_rental, customer, telp, alamat, tujuan, unit, lama, catatan, tanggal, tanggal_pesan, harga } = req.body;
 
-  const query = 'INSERT INTO pesanan_rental (`id_akun`, `id_rental`, `customer`, `telp`, `alamat`, `tujuan`, `unit`, `lama`, `tanggal`, `tanggal_pesan`, `harga`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-  connection.query(query, [id_akun, id_rental, customer, telp, alamat, tujuan, unit, lama, tanggal, tanggal_pesan, harga],(err, results) => {
+  const query = 'INSERT INTO pesanan_rental (`id_akun`, `id_rental`, `customer`, `telp`, `alamat`, `tujuan`, `unit`, `lama`, `catatan`, `tanggal`, `tanggal_pesan`, `harga`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+  connection.query(query, [id_akun, id_rental, customer, telp, alamat, tujuan, unit, lama, catatan, tanggal, tanggal_pesan, harga],(err, results) => {
     if (err) {
       console.error('Error executing query:', err);
       return;
