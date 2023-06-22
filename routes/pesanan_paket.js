2d3e5e1242684335efc7c09ccab36ceae2163ca4
Iqbal Rahman <iqbal070103@gmail.com>
2023-06-22 14:57:20 +0700

    5. Update Stok armada Paket Wisata

--- routes/pesanan_paket.js
+++ routes/pesanan_paket.js
@@ -27,10 +27,10 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  const { id_akun, id_paket, alamat, telp, pax, harga, tanggal, tanggal_pesan } = req.body;
+  const { id_akun, id_paket, alamat, telp, pax, id_rental, harga, tanggal, tanggal_pesan, proses } = req.body;
 
-  const query = 'INSERT INTO pesanan_paket (`id_akun`, `id_paket`, `alamat`, `telp`, `pax`, `harga`, `tanggal`, `tanggal_pesan`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-  connection.query(query, [id_akun, id_paket, alamat, telp, pax, harga, tanggal, tanggal_pesan],(err, results) => {
+  const query = 'INSERT INTO pesanan_paket (`id_akun`, `id_paket`, `alamat`, `telp`, `pax`, `id_rental`, `harga`, `tanggal`, `tanggal_pesan`, `proses`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+  connection.query(query, [id_akun, id_paket, alamat, telp, pax, id_rental, harga, tanggal, tanggal_pesan, proses],(err, results) => {
     if (err) {
       console.error('Error executing query:', err);
       return;
